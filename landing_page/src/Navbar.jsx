e55e87811740ad962cc80bd827f181afd550c4f9
Ilia Pinnecker <ilia.pinnecker@gmail.com>
2024-03-03 17:54:38 +0100

    add changes to navbar and footer

--- landing_page/src/Navbar.jsx
+++ landing_page/src/Navbar.jsx
@@ -1,8 +1,13 @@
 export default function Navbar() {
+
+    const handleClick = () => {
+        window.open('https://www.eckert-coaching.de/', '_blank')
+    }
+
     return (
         <nav className="navbar">
-            <img className="navbar-img" src="./ec-logo-normal.png" alt="eckert-coaching-logo" />
-            <a className="navbar-link" target="_blank" href="https://www.eckert-coaching.de/index.html">Zum Lebenslaufgenerator</a>
+            <img className="navbar-img" onClick={handleClick} src="./ec-logo-normal.png" alt="eckert-coaching-logo" />
+            <a className="navbar-link" target="_blank" href="https://cvas.eckert-coaching.de/cv/">Zum Lebenslaufgenerator</a>
         </nav>
     )
 }
