4a2158a2182d8cf0d6b3b225bd7dadfae7f5f612
Ilia Pinnecker <ilia.pinnecker@gmail.com>
2024-03-03 16:40:54 +0100

    add sticky navbar

--- landing_page/src/Footer.jsx
+++ landing_page/src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer-container">
-                <h3 className="footer-copyright">© 2022 www.eckert-coaching.de. Alle Rechte vorbehalten.</h3>
+                <h3 className="footer-copyright">© 2024 <a className="footer-link" target="_blank" href="https://eckert-coaching.de">Eckert Coaching</a> Alle Rechte vorbehalten.</h3>
                 <a className="footer-link" target="_blank" href="https://eckert-coaching.de/impressum.html">Impressum</a>
                 <a className="footer-link" target="_blank" href="https://eckert-coaching.de/datenschutz.html">Datenschutz</a>
             </div>
